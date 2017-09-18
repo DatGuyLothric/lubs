ae09daa2438d2451da9f6afb392007fb0b3a3992
Denis <31534215+DatGuyLothric@users.noreply.github.com>
2017-09-18 23:20:54 +0300

    lub090417

--- file.c
+++ file.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    float num;
-    char sp;
-    char val;
-    printf("$ tconvert ");
-    scanf("%f%c%c", &num, &sp, &val);
-    switch (val) {
-        case 'C':
-            printf("%.2f F\n", num * 1.8 + 32);
-            printf("%.2f K\n", num + 273.15);
-        break;
-        case 'F':
-            printf("%.2f C\n", (num - 32) / 1.8);
-            printf("%.2f K\n", (num + 459.67) * 5 / 9);
-        break;
-        case 'K':
-            printf("%.2f C\n", num - 273.15);
-            printf("%.2f F\n", num * 9 / 5 - 459.67);
-        break;
-        default:
-            printf("%.2f C:\n", num);
-            printf("%.2f F\n", num * 1.8 + 32);
-            printf("%.2f K\n", num + 273.15);
-            printf("\n");
-            printf("%.2f F:\n", num);
-            printf("%.2f C\n", (num - 32) / 1.8);
-            printf("%.2f K\n", (num + 459.67) * 5 / 9);
-            printf("\n");
-            printf("%.2f K:\n", num);
-            printf("%.2f C\n", num - 273.15);
-            printf("%.2f F\n", num * 9 / 5 - 459.67);
-        break;
+int main(int argc, char* argv[])
+{
+    if (argc == 2)
+    {
+        printf("%.2f C:\n", atof(argv[1]));
+        printf("%.2f F\n", atof(argv[1]) * 1.8 + 32);
+        printf("%.2f K\n", atof(argv[1]) + 273.15);
+        printf("\n");
+        printf("%.2f F:\n", atof(argv[1]));
+        printf("%.2f C\n", (atof(argv[1]) - 32) / 1.8);
+        printf("%.2f K\n", (atof(argv[1]) + 459.67) * 5 / 9);
+        printf("\n");
+        printf("%.2f K:\n", atof(argv[1]));
+        printf("%.2f C\n", atof(argv[1]) - 273.15);
+        printf("%.2f F\n", atof(argv[1]) * 9 / 5 - 459.67);
+    }
+    else
+    {
+        if (strcmp(argv[2], "C") == 0 || strcmp(argv[2], "c") == 0)
+        {
+            printf("%.2f C:\n", atof(argv[1]));
+            printf("%.2f F\n", atof(argv[1]) * 1.8 + 32);
+            printf("%.2f K\n", atof(argv[1]) + 273.15);
+        }
+        else
+        {
+            if (strcmp(argv[2], "F") == 0 || strcmp(argv[2], "f") == 0)
+            {
+                printf("%.2f F:\n", atof(argv[1]));
+                printf("%.2f C\n", (atof(argv[1]) - 32) / 1.8);
+                printf("%.2f K\n", (atof(argv[1]) + 459.67) * 5 / 9);
+            }
+            else
+            {
+                if (strcmp(argv[2], "K") == 0 || strcmp(argv[2], "k") == 0)
+                {
+                    printf("%.2f K:\n", atof(argv[1]));
+                    printf("%.2f C\n", atof(argv[1]) - 273.15);
+                    printf("%.2f F\n", atof(argv[1]) * 9 / 5 - 459.67);
+                }
+            }
+        }
     }
     return 0;
 }
