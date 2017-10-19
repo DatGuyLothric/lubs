68940fd9b49226caffe76b8f313a464a031c3edc
Denis <31534215+DatGuyLothric@users.noreply.github.com>
2017-10-19 15:06:38 +0300

    v1.01

--- calculator.c
+++ calculator.c
@@ -3,24 +3,31 @@
 #include <stdlib.h>
 
 int frstchr;
-int stk[256];
+float stk[256];
 int i = 1;
-int stklmnt;
-int crctn;
+float stkelmnt;
+float crctn;
 int blck = 0;
+int errr = 0;
+int chck = 0;
 
-int pop(void)
+float pop(void) /* Stack function pop */
 {
+    --chck;
+    if (chck < 0)   /* If there are no more elements in stack - Error! */
+        errr = 1;
     return stk[--i];
 };
 
-int push(int stklmnt)
+float push(float stkelmnt)  /* Stack function push */
 {
-    stk[i++] = stklmnt;
+    stk[i++] = stkelmnt;
+    chck++;
 };
 
 int main()
 {
+    printf("This is a reverse polish notation calculator.\nYou can use only: '=' '-' '+' '*' '/' and numbers.\nEnter '=' to get a result.\n");
     do
     {
         frstchr = getchar();
@@ -41,8 +48,14 @@ int main()
             }
             case('-'):
             {
-                crctn = pop();
-                push(pop() - crctn);
+                ungetc(frstchr, stdin);
+                if (scanf("%f", &stkelmnt) != 1)    /* Function checks if the '-' is arithmetic operation or part of number */
+                {
+                    crctn = pop();  /* Have to do a correction cause of stack logic */
+                    push(pop() - crctn);
+                }
+                else
+                    push(stkelmnt);
                 break;
             }
             case('*'):
@@ -53,20 +66,42 @@ int main()
             case('/'):
             {
                 crctn = pop();
+                if (crctn == 0) /* Function checks if divider is not 0 */
+                {
+                    printf("Something went wrong.\nx/0 is not allowed!\n");
+                    return -1;
+                }
+                else
                 push(pop() / crctn);
                 break;
             }
             case('='):
             {
                 blck = 1;
-                printf("The result is %d", pop());
+                if (chck > 1) /* Function checks if there are still some elements in stack */
+                {
+                    printf("Something went wrong.\nCount of arithmetic operations is less then numbers one or the order is wrong.\nCheck what you entered.\n");
+                    return -1;
+                }
+                else
+                    if (errr == 1) /* Print Error if there are no elements in stack */
+                    {
+                        printf("Something went wrong.\nCount of numbers is less then arithmetic operations one or the order is wrong.\nCheck what you entered.\n");
+                        return -1;
+                    }
+                    else
+                        printf("The result is %.2f", pop());
                 break;
             }
             default:
             {
                 ungetc(frstchr, stdin);
-                scanf("%d", &stklmnt);
-                push(stklmnt);
+                if (scanf("%f", &stkelmnt) != 1)    /* Function checks if the element is number or not and push it in to stack if there are no Errors */
+                {
+                    printf("Something went wrong. Check what you entered.\n(Symbols that are not ['=' '-' '+' '*' '/' and numbers] are not allowed!)\nExample: 4 5 - =");
+                    return -1;
+                }
+                push(stkelmnt);
                 break;
             }
         }
