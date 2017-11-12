0217630d1ad69d92d0ae140f9446b9f1ddf82453
Denis <31534215+DatGuyLothric@users.noreply.github.com>
2017-11-12 23:58:58 +0300

    alpha

---
+++ tmachine.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    FILE *fp;
+    char c;
+    int i;
+    int s;
+    char words[1000][50];
+    if ((fp = fopen(argv[1], "r")) == NULL)
+    {
+        printf("No file or incorrect path\n");
+        return -1;
+    }
+    else
+    {
+        for(i = 0; !feof(fp); i++)
+        {
+            fscanf(fp, "%s", &words[i]);
+        }
+    }
+    fclose(fp);
+    int r = i;
+    int bbmx = 0, bbmn = 0;
+    int bb = 0;
+    int ll[bb];
+    ll[0] = 0;
+    for(i = 0; i <= r; i++)
+    {
+        if (strcmp(words[i], "inc") == 0)
+            ll[bb]++;
+        else
+            if (strcmp(words[i], "dec") == 0)
+                --ll[bb];
+            else
+                if (strcmp(words[i], "movl") == 0)
+                {
+                    --bb;
+                    if (bb < bbmn)
+                    {
+                        bbmn = bb;
+                        ll[bb] = 0;
+                    }
+                }
+                else
+                    if (strcmp(words[i], "movr") == 0)
+                        {
+                        bb++;
+                        if (bb > bbmx)
+                            {
+                                bbmx = bb;
+                                ll[bb] = 0;
+                            }
+                        }
+                    else
+                        if (strcmp(words[i], "print") == 0)
+                            printf("%d\n", ll[bb]);
+                        else
+                            if (strcmp(words[i], "begin") == 0)
+                            else
+                                if (strcmp(words[i], "end") == 0)
+    }
+    return 0;
+}
