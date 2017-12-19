e147265fc874b00905414ff28d3919137fa75027
Denis <31534215+DatGuyLothric@users.noreply.github.com>
2017-12-20 00:58:05 +0300

    v1.02

--- filefind.c
+++ filefind.c
@@ -5,58 +5,61 @@
 int main(int argc, char* argv[])
 {
     FILE *fp;
-    int i = 0, b = 0, n = 1, s = 1, p = 2, t, g = 0, l = 0, k = 0;
-    char c, w[1000], f[999999];
+    int i = 0, j = 0, s_num = 1, str = 1, check = 2, f_sym_c, ii = 0, jj = 0, extr = 0;
+    char c, s_word[1000], f_sym[999999];
     printf("Enter word for search: ");
-    while ((c = getchar()) != '\n')
+    while ((c = getchar()) != '\n') /* Getting word for search */
     {
-        w[b] = c;
-        b++;
+        s_word[j] = c;
+        j++;
     }
-    if ((fp = fopen(argv[1], "r")) == NULL)
+    if ((fp = fopen(argv[1], "r")) == NULL) /* Checking if the file exists */
     {
         printf("No file or incorrect path\n");
         return -1;
     }
     else
     {
-        while (!feof(fp))
+        while (!feof(fp))   /* Getting file symbols */
         {
-            f[i] = getc(fp);
+            f_sym[i] = getc(fp);
             i++;
         }
-        t = i;
+        f_sym_c = i;
     }
-    while ((g <= t) && (p != 1))
+    while ((ii <= f_sym_c) && (check != 1))
     {
-        p = 2;
-        if (f[g] == '\n')
+        check = 2;
+        if (f_sym[ii] == '\n')  /* Incr line or number of element if needed */
         {
-            s++;
-            n = 0;
+            str++;
+            s_num = 0;
         }
-        else if (f[g] == ' ')
+        else if (f_sym[ii] == ' ')
         {
-            n++;
+            s_num++;
         }
         else
-        {l = 0; while (((g - k) <= b - 1) && (p != 0))
         {
-            if (w[l] == f[g])
+            jj = 0;
+            while (((ii - extr) <= j - 1) && (check != 0))  /* Checking if it is the word we need */
             {
-                p = 1;
-                l++;
-                g++;
+                if (s_word[jj] == f_sym[ii])
+                {
+                    check = 1;
+                    jj++;
+                    ii++;
+                }
+                else
+                    check = 0;
             }
-            else
-                p = 0;
-        }}
-        k++;
-        g = k;
+        }
+        extr++;
+        ii = extr;
     }
     fclose(fp);
-    if (p == 1)
-        printf("Line is %d, Number in line is %d", s, n);
+    if (check == 1)
+        printf("Line is %d, Number in line is %d", str, s_num++);
     else
         printf("There is no such word in this file!");
     return 0;
